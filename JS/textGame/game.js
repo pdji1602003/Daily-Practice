316d15f329e666440f8cf62de5a0fc44e149ed97
pdji1602003 <pdji1602003@gmail.com>
2019-09-28 19:40:15 +0800

    Update game.js
    更新重點：
    1. 針對每個function下說明註解
    2. 透過debugger了解每個function的作用

--- JS/textGame/game.js
+++ JS/textGame/game.js
@@ -6,9 +6,13 @@ textElement內容會改變
 const textElement = document.getElementById('text');
 const optionButtonsElement = document.getElementById('option-buttons');
 
+
+
+//state是用來幹嘛的呢？這是用let聲明的變量，代表其數值之後會再更改
 let state = {};
 
 function startGame(){
+  //debugger 使用debugger去看一個每function到底在做什麼
   state = {};
   showTextNode(1);
 }
@@ -18,13 +22,43 @@ function showTextNode(textNodeIndex){
   array是一群兄弟，他們擁有共同的侍僕，侍僕們個個具備奇怪的個性，
   其中一位名為find。*/
   
-  const textNode = textNodes.find(textNode =>
+  const textNode = textNodes.find(textNode => //textNodes的每個元素們都是object
+    /*
+    找到滿足以下條件的第一個textNode，條件可任意。
+    比方説若我想使用者點擊網頁進後來，看到的第一段text是'You venture forth...'
+    我可以將條件改為textNode.options[0].nextText === textNodeIndex(3) 
+    */
     textNode.id === textNodeIndex);
+    
+    //從textNodes裡找尋符合id是1的元素並返回，
+    //取用id是1元素的text
     textElement.innerText = textNode.text;
+
+
+    /* 1.
+    一開始怎麼也看不明白，為何while(condition)可以接受optionButtonsElement.firstChild為condition，
+    let result = optionButtonsElement.firstChild;
+    console.log(result)不會等於true啊
+    結果再次去看了文檔，while的condition可以是任何表达式或变量都可以是循环条件，而不仅仅是比较。
+    while會对它们进行计算，并将其结果转化为布尔值。
+    
+    換句話說，只要condition尚未是falsy值，code block裡的code都會被執行。(直到condition裡的值變成falsy)
+    *備註：這可能是跟for...loop比較不一樣的地方。(for...loop裡通常使用比較，而while是將真值假值作為condition)
+    在以下這個例子中，optionElement.firstChild取得的值不是button.btn就是text，他們都不是falsy value，
+    因此optio...Element.removeChild會一直移除，直到將所有的孩子們都清除。
+    都清除的狀況之下，optionButtonsElement.firstChild會變成null，表示沒有取得任何東西。
+    null是falsy value，條件為假，while循環結束。
+    */
+
+    /* 2.
+    順帶一提，optionButtonsElement之所以要清除小孩們，
+    是因為使用者點按後，需要將所有按鈕清除，再生成符合使用使用者選擇的按扭。
+    這邊使用的是firstChild才可以清除element以及文本本身。
+    */
     while(optionButtonsElement.firstChild){
       optionButtonsElement.removeChild(optionButtonsElement.firstChild);
     }
-
+    //當下面showOption(option)返回真值時，if條件語句就會運行，button就會被創造。
     textNode.options.forEach(option => {
       if(showOption(option)){
         const button = document.createElement('button');
@@ -36,9 +70,14 @@ function showTextNode(textNodeIndex){
     })
 }
 
+/*
+是真值的條件是什麼？先想一下再什麼情況不用產生button
+*/
 function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
+  // console.log(option.requiredState == null || option.requiredState(state)); 返回值為true
+  return option.requiredState == null || option.requiredState(state)//或運算尋找第一個真值
 }
+
 
 function selectOption(option) {
   const nextTextNodeId = option.nextText
@@ -51,6 +90,7 @@ function selectOption(option) {
 
 // 任何文本在網頁裡都被視為是textNode
 // 將此頁面需要用到的全部text都以array存進這個textNodes變量
+
 const textNodes = [
   {
     id: 1,
@@ -58,8 +98,8 @@ const textNodes = [
     options: [
       {
         text: 'Take the goo',
-        setState: { blueGoo: true },
-        nextText: 2
+        setState: { blueGoo: true }, //我猜，這是指使用者目前透過選項所獲取到的物品，true表示使用者目前擁有blueGoo
+        nextText: 2 //指向下一個text的id
       },
       {
         text: 'Leave the goo',
@@ -73,8 +113,8 @@ const textNodes = [
     options: [
       {
         text: 'Trade the goo for a sword',
-        requiredState: (currentState) => currentState.blueGoo,
-        setState: { blueGoo: false, sword: true },
+        requiredState: (currentState) => currentState.blueGoo,//檢查使用者目前是否有blueGoo?
+        setState: { blueGoo: false, sword: true },//使用者目前沒有blueGoo，但有sword
         nextText: 3
       },
       {
@@ -95,7 +135,7 @@ const textNodes = [
     options: [
       {
         text: 'Explore the castle',
-        nextText: 4
+        nextText: 4 
       },
       {
         text: 'Fine a room to sleep at in the town',
@@ -204,4 +244,13 @@ const textNodes = [
   }
 ]
 
+//畫面一load進來，即調用startGame()，讓畫面已經存在遊戲文字
 startGame()
+
+//game.js:54 Uncaught ReferenceError: Cannot access 'textNodes' before initialization
+/* 若試圖在cost textNodes被聲明之前取值，若跑出以上那段錯誤，直譯就是
+無法在初始化前取得textNodes，可是textNodes明明就有在game:21被使用了啊？為何會這樣？
+*/
+console.log(textNodes[0].id);
+
+console.log(textNodes[1].options[1].requiredState);
