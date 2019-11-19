7c659673c5fd6a96832b1fa4607d1e79952e95f6
pdji1602003 <pdji1602003@gmail.com>
2019-11-20 00:35:49 +0800

    react first commit

---
+++ React/my-app/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TodoItem(){
+	return (
+		<div>
+			<input type="checkbox"/>
+			<p>todo item here</p>
+		</div>
+	)
+}
+
+
+export default TodoItem
---
+++ React/my-app/src/index.js
@@ -0,0 +1,45 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+
+// React Inline Styles with style property
+// function App(){
+//   const time = new Date()
+//   const hour = time.getHours()
+//   let timeOfDay
+//   const styles = {
+//     fontSize : 24
+//   }
+
+//   if(hour < 12 ){
+//     timeOfDay = 'Good Morning!'
+//     styles.color = "#124452"
+//   } else if(hour >= 12 && hour < 17){
+//     timeOfDay = 'Good Afternoon!'
+//     styles.color = "#12d234"
+//   } else {
+//     timeOfDay = 'Good Evening!'
+//     styles.color = "#aa3b35"
+//   }
+//   // 把style的值以物件型態儲存，在透過{}花括號設置在h1 tag上
+//   return (
+//     <h1 style={styles}>{timeOfDay}</h1>
+//   )
+// }
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TodoItem from './components/TodoItem'
+
+function App(){
+  return (
+    <div>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
