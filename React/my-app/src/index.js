0fc71115a47dea7dc7a7dca934d7785c5c87a17c
pdji1602003 <pdji1602003@gmail.com>
2019-12-16 21:38:16 +0800

    update

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,34 +1,12 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// React Inline Styles with style property
-// function App(){
-//   const time = new Date()
-//   const hour = time.getHours()
-//   let timeOfDay
-//   const styles = {
-//     fontSize : 24
-//   }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-//   if(hour < 12 ){
-//     timeOfDay = 'Good Morning!'
-//     styles.color = "#124452"
-//   } else if(hour >= 12 && hour < 17){
-//     timeOfDay = 'Good Afternoon!'
-//     styles.color = "#12d234"
-//   } else {
-//     timeOfDay = 'Good Evening!'
-//     styles.color = "#aa3b35"
-//   }
-//   // 把style的值以物件型態儲存，在透過{}花括號設置在h1 tag上
-//   return (
-//     <h1 style={styles}>{timeOfDay}</h1>
-//   )
-// }
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<App/>, document.getElementById('root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
